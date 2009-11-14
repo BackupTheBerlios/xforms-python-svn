8534cc42185106c648b9f2d2b968f9abd5cdec10
lukenshiro <lukenshiro@dd7693d3-9477-0410-adb9-c7d5d079a054>
2009-11-14 09:57:42 +0000

    add some doc files and setup script

---
+++ trunk/setup.py
@@ -0,0 +1,23 @@
+import sys
+import distutils.core
+
+long_description = """
+xforms-python - Python wrapper for XForms X11 Windows System GUI C toolkit.
+"""
+
+distutils.core.setup(name = 'xforms-python',
+                     version = "0.1.0_1.0.92sp2",
+                     description = 'XForms python wrapper',
+                     long_description = long_description,
+                     author = 'Luca Lazzaroni (LukenShiro)',
+                     author_email = 'lukenshiro@ngi.it',
+                     license = 'LGPLv2.1',
+                     platforms = 'POSIX',
+                     keywords = 'xforms python wrapper',
+                     url = 'http://xforms-python.berlios.de',
+                     py_modules=['xformslib'],
+                     data_files=[('share/doc/xforms-python-'+version, \
+                                  ['doc/ChangeLog', \
+                                  'doc/lgpl-2.1.txt', 'doc/INSTALL', \
+                                  'doc/README', 'doc/TODO'])],
+                     )
