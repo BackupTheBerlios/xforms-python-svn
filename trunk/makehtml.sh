f5452904ad61d4e2fc93186f7ec153c03e567e22
lukenshiro <lukenshiro@dd7693d3-9477-0410-adb9-c7d5d079a054>
2010-12-09 01:08:49 +0000

    adjust some html files, remove internal (unused) directory

--- trunk/makehtml.sh
+++ trunk/makehtml.sh
@@ -5,8 +5,8 @@ cd doc/html
 
 pydoc -w ../../xformslib/*.py
 # hack to display common use modules and remove local path
-sed -i "s|xformslib.||g" fl*.html
+sed -i "s|xformslib.||g" [xflv_]*.html
 sed -i "s|<a href=\"file:/.*</a></font></td></tr></table>$| \
-    <a href=\"index.html\">list</a></font></td></tr></table>|" [xflv]*.html
+    <a href=\"index.html\">list</a></font></td></tr></table>|" [xflv_]*.html
 
 cd ../..
