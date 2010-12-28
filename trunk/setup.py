680d736d32f2e65c8b7e1e0ed609f8ea484eb448
lukenshiro <lukenshiro@dd7693d3-9477-0410-adb9-c7d5d079a054>
2010-12-28 23:36:52 +0000

    bugfix in setup.py

--- trunk/setup.py
+++ trunk/setup.py
@@ -10,11 +10,11 @@ long_description = """
 xforms-python - Python wrapper for XForms X11 Window System GUI C toolkit.
 """
 doc_files = ['doc/ChangeLog', 'doc/CREDITS', 'doc/Docs.txt', 'doc/INSTALL', \
-	    'doc/lgpl-2.1.txt', 'doc/LICENSE', 'doc/README', 'doc/TODO', 'doc/USAGE']
+        'doc/lgpl-2.1.txt', 'doc/LICENSE', 'doc/README', 'doc/TODO', 'doc/USAGE']
 html_files = glob.glob('doc/html/*.html')
-examples_files = glob.glob('examples/*.py') + glob.glob('examples/*.x?m')
-examples_files.append('examples/Readme')
-bin_files = ['bin/fd2python.py',]
+example_files = glob.glob('examples/*.py') + glob.glob('examples/*.x?m')
+example_files.append('examples/Readme')
+bin_files = 'bin/fd2python.py'
 
 setup(name = name,
     version = version,
@@ -26,10 +26,10 @@ setup(name = name,
     platforms = 'POSIX',
     keywords = 'xforms python wrapper',
     url = 'http://xforms-python.berlios.de',
-    packages=['xformslib'],
-    package_dir={'xformslib' : 'xformslib'},
-    data_files=[(os.path.join('doc/xforms-python-', version), doc_files), \
-            (os.path.join('doc/xforms-python-', version, "html"), html_files), \
-            (os.path.join('share/xforms-python-', version), examples_files), \
-            ('bin', bin_files)]
+    packages = ['xformslib'],
+    package_dir = {'xformslib' : 'xformslib'},
+    scripts = [bin_files, ],
+    data_files = [('share/doc/xforms-python-'+version, doc_files), \
+            (os.path.join('share/doc/xforms-python-'+version, "html"), html_files), \
+            ('share/xforms-python-'+version, example_files)]
     )
